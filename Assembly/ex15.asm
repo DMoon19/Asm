eab92e141b74fc4f5c22cf30884501fd3907747d
David García <81989868+DMoon19@users.noreply.github.com>
2023-02-02 15:53:26 +0000

    Traduccion a asm

---
+++ Assembly/ex15.asm
@@ -0,0 +1,6 @@
+int i = 1;
+int sum = 0;
+While (i <= 100){
+  sum += i;
+  i++;
+}
---
+++ Assembly/prog.asm
@@ -0,0 +1,6 @@
+//int i = 1;
+@i        // A = 16
+M = 1     // MEMORY[A] = 1
+//int sum = 0;
+@sum      // A = 17
+M = 0     // MEMORY[A] = 0
